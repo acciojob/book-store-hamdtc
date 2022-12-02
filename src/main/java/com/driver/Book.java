0172246883648c835a124ddb9f4a24fce1a87943
hamdtc <mohamedtc73@gmail.com>
2022-12-03 01:27:02 +0530

    third commit

--- src/main/java/com/driver/Book.java
+++ src/main/java/com/driver/Book.java
@@ -13,6 +13,12 @@ public class Book {
      public Book(){
 
      }
+     public Book(String name, String genre, String author) {
+          this.id = id;
+          this.name = name;
+          this.genre = genre;
+          this.author = author;
+     }
 
      public Book(int id, String name, String genre, String author) {
           this.id = id;
